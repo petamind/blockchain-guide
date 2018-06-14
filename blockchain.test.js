a6ebf78f255246d62ace4944732987f03065366b
Doan Tung Nguyen <doan@nzse.ac.nz>
2018-06-15 11:00:35 +1200

    add test chain validation

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,10 +2,11 @@ const Block = require('./block');
 const Blockchain = require('./blockchain');
 
 describe('Blockchain', () => {
-    let bc;
+    let bc, bc2;
 
     beforeEach(() => {
         bc = new Blockchain();
+        bc2 = new Blockchain();
     });
 
     it('start with genesis block', () => {
@@ -16,5 +17,21 @@ describe('Blockchain', () => {
         const data = 'foo';
         bc.addBlock(data);
         expect(data).toEqual(bc.chain[bc.chain.length -1].data);
+    });
+
+    it('validates a valid chain', () => {
+        bc2.addBlock('foo');
+        expect(bc.isValidChain(bc2.chain)).toBe(true);//check itself valid?
+    });
+
+    it('invalidates a chain with a currupt genesis block', () => {
+        bc2.chain[0].data = 'Bad data';
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('invalidates a corrupted chain', () => {
+        bc2.addBlock('foo');
+        bc2.chain[1].data = 'Not foo';
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
     });
 });
